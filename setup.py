35856b16cd1f08cb7d2789f3783cf695f2cbd011
AlexandrovRoman <56388249+AlexandrovRoman@users.noreply.github.com>
2020-03-01 18:26:37 +0400

    Fix bug in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.md'), encoding='utf-8') as f:
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     requirements = f.readlines()
 
 setup(

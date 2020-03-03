254f2295254edf0751c6d7036692e693fdc7a188
AlexandrovRoman <56388249+AlexandrovRoman@users.noreply.github.com>
2020-03-03 22:29:53 +0400

    Refactoring project and add docs

--- Flask_DJ/__init__.py
+++ Flask_DJ/__init__.py
@@ -1,10 +1,10 @@
-from os import makedirs, mkdir
 from Flask_DJ.exceptions import CreationError
-from Flask_DJ.templates import config_file, init_file, urls_file, manage_file
+from Flask_DJ.templates import config_file, init_file, urls_file, manage_file, utils_urls
 from string import ascii_letters
 from random import choices, randint
 from sys import argv
-from .utils import create_file
+from .docs import urls_docs, manage_docs
+from .utils import create_file, create_folder, format_docs
 
 
 class ProjectConstructor:
@@ -43,16 +43,16 @@ class ProjectConstructor:
                                        secret_key=self.generate_key(randint(9, 12))))
 
     def _create_urls(self):
-        create_file(self.main_app_path, 'urls', urls_file.format(project_name=self.project_name))
+        create_file(self.main_app_path, 'urls', urls_file.format(docs=format_docs(urls_docs)))
 
     def _create_main_app_folder(self):
-        mkdir(self.main_app_path)
+        create_folder(self.main_app_path)
 
     def _create_templates(self):
-        mkdir(f'{self.project_path}\\templates')
+        create_folder(f'{self.project_path}\\templates')
 
     def _create_static(self):
-        mkdir(f'{self.project_path}\\static')
+        create_folder(f'{self.project_path}\\static')
 
     def _create_main(self):
         self._create_main_app_folder()
@@ -61,16 +61,17 @@ class ProjectConstructor:
         self._create_urls()
 
     def _create_manage(self):
-        create_file(self.path, 'manage', manage_file.format(project_name=self.project_name))
+        create_file(self.path, 'manage',
+                    manage_file.format(docs=format_docs(manage_docs), project_name=self.project_name))
 
     def _create_project_folder(self):
-        try:
-            makedirs(self.project_path)
-        except FileExistsError:
-            raise CreationError('The directory with that name already exists.')
-        except PermissionError:
-            raise CreationError('You do not have enough permissions to act in this directory, '
-                                'try changing users and try again.')
+        create_folder(self.project_path)
+
+    def _create_utils_folder(self):
+        create_folder(f'{self.project_path}\\utils')
+
+    def _create_utils_urls(self):
+        create_file(f'{self.project_path}\\utils', 'urls', utils_urls.format(project_name=self.project_name))
 
     def startproject(self):
         self._create_project_folder()
--- Flask_DJ/utils.py
+++ Flask_DJ/utils.py
@@ -1,3 +1,25 @@
+from os import makedirs, getcwd
+from Flask_DJ.exceptions import CreationError
+
+
 def create_file(path, name, template):
     with open(f'{path}\\{name}.py', 'w') as f:
         f.write(template)
+
+
+def create_folder(path):
+    try:
+        makedirs(path)
+    except FileExistsError:
+        raise CreationError('The directory with that name already exists.')
+    except PermissionError:
+        raise CreationError('You do not have enough permissions to act in this directory, '
+                            'try changing users and try again.')
+
+
+def get_project_name():
+    return getcwd().split('\\')[-1]
+
+
+def format_docs(docs):
+    return '"""' + docs + '"""'

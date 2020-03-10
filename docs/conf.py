26b332eb10ad4a9d509dfe2b88c00b0b4e49ebde
AlexandrovRoman <56388249+AlexandrovRoman@users.noreply.github.com>
2020-03-10 22:01:23 +0400

    Update project version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Alexandrov Roman'
 author = 'Alexandrov Roman'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.5'
+release = '0.1.6'
 
 
 # -- General configuration ---------------------------------------------------

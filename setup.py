589a7a7827108f3e1340901d54b44e9b825bfe3e
AlexandrovRoman <56388249+AlexandrovRoman@users.noreply.github.com>
2020-03-30 18:26:30 +0400

    Fix bugs in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, find_packages
-from flask_dj import __version__, __title__, __description__, __author_email__, __author__, __keywords__, __license__
+from setuptools import setup
 from os import path
 
 here = path.abspath(path.dirname(__file__))
@@ -8,29 +7,12 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'PyPI_README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(
-    name=__title__,  # Required
-    version=__version__,  # Required
-    description=__description__,  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-    url='https://github.com/AlexandrovRoman/Flask-DJ',  # Optional
-    author=__author__,  # Optional
-    author_email=__author_email__,  # Optional
-    classifiers=[  # Optional
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        f'License :: OSI Approved :: {__license__}',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    keywords=__keywords__,  # Optional
-    packages=find_packages(),  # Required
-    install_requires=[
+about = {}
+with open(path.join(here, 'flask_dj', '__project_info__.py')) as f:
+    exec(f.read(), about)
+
+packages = ["flask_dj"]
+requirements = [
         'waitress>=1.4.3',
         'flask>=1.1.1',
         'Flask-Migrate>=2.5.2',
@@ -40,10 +22,34 @@ setup(
         'SQLAlchemy>=1.3.13',
         'Flask-WTF>=0.14.3',
         'Flask-Login>=0.5.0'
+    ]
+
+setup(
+    name=about["__title__"],  # Required
+    version=about["__version__"],  # Required
+    description=about["__description__"],  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/AlexandrovRoman/Flask-DJ',  # Optional
+    author=about["__author__"],  # Optional
+    author_email=about["__author_email__"],  # Optional
+    classifiers=[  # Optional
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        f'License :: OSI Approved :: {about["__license__"]}',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
+    keywords=about["__keywords__"],  # Optional
+    packages=packages,  # Required
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'flask-dj = Flask_DJ:command',
+            'flask-dj = flask_dj:command',
         ],
     },
     project_urls={
